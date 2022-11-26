ce5c62ee950d0b5c47c1a1a496a2c0de39b35903
Shakhawat2 <shohugmahbub@gmail.com>
2022-11-26 11:56:36 +0600

    create data for specific category

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -16,14 +16,22 @@ async function run() {
     try {
         const usersCollection = client.db("assignment-12").collection("user");
         const categoryCollection = client.db("assignment-12").collection("category");
-        
-        // get category 
+        const category_Product_Collection = client.db("assignment-12").collection("category_product");
+
+        // get all category 
         app.get('/category', async (req, res) => {
             const query = {};
             const categories = await categoryCollection.find(query).toArray();
             res.send(categories);
         })
-
+        // get category product 
+        app.get('/category/:id', async (req, res) => {
+            const id = parseInt(req.params.id);
+            const query = { category_id : id};
+            const product = await category_Product_Collection.find(query).toArray();
+            console.log(product);
+            res.send(product);
+        })
 
         // Post user
         app.post('/users', async (req, res) => {
